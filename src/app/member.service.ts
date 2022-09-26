8a80b5108900bc8f382f47f1aad6875a582b8293
Akihiro.Shiozawa <shioshio.0111@gmail.com>
2022-09-26 23:00:12 +0900

    add message function

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { MEMBERS } from './mock-members';
 import { Member } from './member';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getMembers(): Observable<Member[]> {
+    this.messageService.add("MemberService: 社員データを取得しました");
     return of(MEMBERS);
   }
 }
